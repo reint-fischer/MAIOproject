1937edfac55995cc8b890852964cca40ba59fefc
Reint <33000433+reint-fischer@users.noreply.github.com>
2019-10-11 09:47:02 +0200

    Add two data reading options
    Using either netCDF4 or xarray, read the data. Then try to downsize the dataset to drifters in the Mediterrenean

--- readdata.py
+++ readdata.py
@@ -7,11 +7,33 @@ Created on Wed Oct  9 10:22:58 2019
 @author: Gebruiker
 """
 from netCDF4 import Dataset,num2date
+import numpy as np
+import xarray as xr
 
-def OpenDrifterdata(filelocation):
+def OpenDrifterdata(filelocation): #Open netcdf file and unpack relevant parameters
     file = Dataset(filelocation,'r+',format = 'NETCDF4')
-    time = num2date(file.variables['time'][:],file.variables['time'].units)
-    latitude = file.variables['lat'][:]
-    longitude = file.variables['lon'][:]
-    ID = file.variables['ID'][:]
+    time = file.variables['TIME'][:] #timestamp of GPS location
+    latitude = file.variables['LAT'][:] #latitude of corresponding drifter ID at corresponding time
+    longitude = file.variables['LON'][:] #longitude of corresponding drifter ID at corresponding time
+    ID = file.variables['ID'][:] 
+    return ID,time,latitude,longitude,time
 
+def selectMedsea(ID,lat,lon):
+    newID = []
+    newlat = []
+    newlon = []
+    for i in range(len(lat)):
+        if not(ID[i] in newID):
+            if lat[i]<45 and lat[i]>30 and lon[i]<36 and lon[i]>5:
+                newID += [ID[i]]
+                newlat += [lat[i]]
+                newlon += [lon[i]]
+    return np.array((newID,newlat,newlon))
+
+
+#%%
+if __name__ == '__main__':
+    ID,time,latitude,longitude,time = OpenDrifterdata('C:/Users/Gebruiker/Downloads/driftertrajGPS_1.03.nc')
+#    #%%
+#    nd = selectMedsea(ID,latitude,longitude)
+#    np.savetxt('MedSeaIDs.csv',nd,delimiter=',')
