5d634206b9500e72605ce3b0e8bc516126adc2ed
Giovanni7779 <55230060+Giovanni7779@users.noreply.github.com>
2019-10-15 10:00:18 +0200

    Create UnPairing.py

---
+++ UnPairing.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+Pair = np.genfromtxt('Pair.txt', delimiter=',')
+
+
+k = 0 
+UnPair = np.zeros([50,2])
+for i in range (0,70):
+    A = Pair[i]
+    B = [A[1] ,A[0]]
+    if not (B in Pair): print('Fuck it') #There's always another
+    if not( A in UnPair) : 
+       if not ( B in UnPair) : 
+          UnPair[k] = A
+          k= k+1
+
+np.savetxt('UnPair.txt',UnPair,delimiter=',')
