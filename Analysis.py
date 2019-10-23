48afabc117b712ccaf0db85d29a86b045a37fc57
Reint <33000433+reint-fischer@users.noreply.github.com>
2019-10-23 11:32:19 +0200

    FSLE comp

--- Analysis.py
+++ Analysis.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 DirecInputForward = "Data/ForwardDistances/"
 DirecInputBackward = "Data/BackwardsDistances/"
+pairs = np.genfromtxt('Data/UnPair.txt', delimiter=',')
 
 def func(x, a, b, c):
    return a * math.pow(x, -2/b) + c   #THIS FOR THE FIT -> IT DOESN'T WORK
@@ -15,30 +16,45 @@ Pair = []
 MaxLenPair = 0        #-> For localizing the dataset which is the longest in the group
 MaxLenChancePair = 0
 
+ChancePairb = []       #-> Two list, one with the pairs and one with the chancepairs
+Pairb = []
+MaxLenPairb = 0        #-> For localizing the dataset which is the longest in the group
+MaxLenChancePairb = 0
 
 for i in range (0,33):
-   A = np.genfromtxt(DirecInputForward+'FDPair'+str(i)+".csv", delimiter=',')/1000  #It read the file and copy in A
-   if min(A[0][:]) < 1 :                                                       #If the minimum value is below 1km (squared) it's a pair
-      Pair.append(A)
-      B = len(A[0][:])
-      if B > MaxLenPair :
-         MaxLenPair = B 
-   if min(A[0][:]) > 1 :                                                       #If the minimum distance is above 1km (squared) it's a chancepair
-      ChancePair.append(A)
-      B = len(A[0][:])
-      if B > MaxLenChancePair :
-         MaxLenChancePair = B 
+    Ab = np.genfromtxt(DirecInputBackward+'BDPair'+str(i)+".csv", delimiter=',')  #It read the file and copy in A
+    A = np.genfromtxt(DirecInputForward+'FDPair'+str(i)+".csv", delimiter=',')  #It read the file and copy in A
+    if len(Ab.shape) > 1:
+        ChancePairb.append(Ab)
+        ChancePair.append(A)
+        Bb = len(Ab[0][:])
+        B = len(A[0][:])
+        if Bb > MaxLenChancePairb :
+            MaxLenChancePairb = Bb
+        if B > MaxLenChancePair :
+            MaxLenChancePair = B
+    else:
+        Pairb.append(Ab)
+        Bb = 1
+        if Bb > MaxLenPairb :
+            MaxLenPairb = Bb
+        Pair.append(A)
+        B = len(A[0][:])
+        if B > MaxLenPair :
+            MaxLenPair = B
+        
+    
 
 ############################---FSLE---################################
 
 
-do = 1.5 #[meters]
-num = 65 #Number of resolution FSLE
+do = 1000 #[meters]
+num = 70 #Number of resolution FSLE
 r = 1.1 #Grid increment
 
 GridGoals = np.zeros([num])
 GridGoals[0] = do
-FSLE = np.zeros([3,num])         #I would like to keep the second row as index of how many pairs compute that mean, you see what I mean?
+FSLE = np.zeros([3,num,2])         #I would like to keep the second row as index of how many pairs compute that mean, you see what I mean?
 
 for i in range (0,num-1) :
    GridGoals[i+1] = GridGoals[i]*r
@@ -51,9 +67,9 @@ for i in range(0,num) :
       A = ChancePair[t]
       for k in range(1,len(A[0][:])) : 
          if A[0][k]> GridGoals[i] and A[0][k-1]< GridGoals[i]: 
-            Time = Time + (A[1][k]-A[1][0])
+            Time = Time + (np.abs(A[1][k]-A[1][0]))
             n = n+1 
-            Errors.append(A[1][k]-A[1][0])
+            Errors.append(np.abs(A[1][k]-A[1][0]))
             break
    if n != 0 :
       FSLE[0,i] = math.pow(Time/n,-1)*math.log(r)
@@ -71,27 +87,111 @@ for i in range(0,num) :
 #for i in range (0,num):
 #   Model[i] = popt[0] * math.pow(x, -2/popt[1]) + popt[2]
 #print(popt)
+   
+Errorsb=[]
+for i in range(0,num) :
+   nb = 0
+   Timeb = 0 
+   for t in range(0,len(ChancePairb)) : 
+      Ab = ChancePairb[t]
+      for k in range(1,len(Ab[0][:])) : 
+         if Ab[0][k]> GridGoals[i] and Ab[0][k-1]< GridGoals[i]: 
+            Timeb = Timeb + (np.abs(Ab[1][k]-Ab[1][0]))
+            nb = nb+1 
+            Errorsb.append(np.abs(Ab[1][k]-Ab[1][0]))
+            break
+   if nb != 0 :
+      FSLE[0,i,1] = math.pow(Timeb/nb,-1)*math.log(r)
+      FSLE[1,i,1] = nb
+      Varb = 0 
+      for p in range(0,len(Errorsb)) :
+         Varb = Varb + ((Timeb/nb - Errorsb[p])*(Timeb/nb - Errorsb[p]))/nb    
+      FSLE[2,i,1] = math.pow(Varb,0.5) * math.pow(Timeb/nb,-2)*math.log(r)
+   else :
+      print("Warning  :",i)
+   while len(Errorsb) > 0 : Errorsb.pop()
 
 xError = np.zeros(num) #Error space (equal to zero)
-fig, ax1 = plt.subplots()
+
+
+###Forward###
+f1 = plt.figure(1)
+ax1 = plt.axes()
+plt.title('Forward FSLE')
 color = 'tab:red'
 ax1.set_xlabel('Distance (km)')
 #ax1.set_yscale('log')
 ax1.set_ylabel('FSLE', color=color)
-ax1.loglog(GridGoals, FSLE[0,:], color=color)
-ax1.errorbar(GridGoals, FSLE[0,:],
+ax1.loglog(GridGoals/1000, FSLE[0,:,0], color=color)
+ax1.errorbar(GridGoals/1000, FSLE[0,:,0],
             xerr=xError,
-            yerr=FSLE[2,:],
+            yerr=FSLE[2,:,0],
             fmt='-o', color='red')
 ax1.tick_params(axis='y', labelcolor=color)
 
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+ax12 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
-ax2.set_ylabel('Components', color=color)  # we already handled the x-label with ax1
-ax2.plot(GridGoals, FSLE[1,:], color=color)
+ax12.set_ylabel('Components', color=color)  # we already handled the x-label with ax1
+ax12.plot(GridGoals/1000 , FSLE[1,:,0], color=color)
+ax12.tick_params(axis='y', labelcolor=color)
+
+f1.tight_layout()  # otherwise the right y-label is slightly clipped
+
+###Backward###
+f2 = plt.figure(2)
+ax2 = plt.axes()
+plt.title('Backward FSLE')
+color = 'tab:red'
+ax2.set_xlabel('Distance (km)')
+#ax1.set_yscale('log')
+ax2.set_ylabel('FSLE', color=color)
+ax2.loglog(GridGoals/1000, FSLE[0,:,1], color=color)
+ax2.errorbar(GridGoals/1000, FSLE[0,:,1],
+            xerr=xError,
+            yerr=FSLE[2,:,1],
+            fmt='-o', color='red')
 ax2.tick_params(axis='y', labelcolor=color)
 
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
-plt.show()
+ax22 = ax2.twinx()  # instantiate a second axes that shares the same x-axis
 
+color = 'tab:blue'
+ax22.set_ylabel('Components', color=color)  # we already handled the x-label with ax1
+ax22.plot(GridGoals/1000 , FSLE[1,:,1], color=color)
+ax22.tick_params(axis='y', labelcolor=color)
+
+f2.tight_layout()  # otherwise the right y-label is slightly clipped
+
+###Comparison###
+f3 = plt.figure(3)
+ax3 = plt.axes()
+plt.title('Symmetry')
+color = 'tab:red'
+ax3.set_xlabel('Distance (km)')
+#ax1.set_yscale('log')
+ax3.set_ylabel('FSLE')
+ax3.loglog(GridGoals/1000, FSLE[0,:,0], color=color)
+ax3.errorbar(GridGoals/1000, FSLE[0,:,0],
+            xerr=xError,
+            yerr=FSLE[2,:,0],
+            fmt='-o', color='red')
+
+color = 'tab:green'
+ax3.loglog(GridGoals/1000, FSLE[0,:,1], color=color)
+ax3.errorbar(GridGoals/1000, FSLE[0,:,1],
+            xerr=xError,
+            yerr=FSLE[2,:,1],
+            fmt='-o', color='green')
+
+
+############################---Diffusivity---################################
+K = np.zeros((len(Pair),MaxLenPair,2))
+f4 = plt.figure(4,figsize=(10,8))
+ax4 = plt.axes()
+ax4.set_ylabel('Diffusivity K [$m^2$/s]')
+ax4.set_xlabel('Distance (km)')
+for i in range(len(Pair)):
+    for j in range(len(Pair[i][0])-1):
+        K[i,j,0] = (Pair[i][0][j+1]**2-Pair[i][0][j]**2)/3600/2
+        K[i,j,1] = (Pair[i][0][j]+Pair[i][0][j+1])/2
+    ax4.loglog(K[i,:,1]/1000,K[i,:,0],'o',label = 'floats {0} and {1}'.format(int(pairs[i,0]),int(pairs[i,1])))
