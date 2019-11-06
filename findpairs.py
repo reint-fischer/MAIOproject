fbea8007f30904087c17830272256e7fdcc88bde
Reint <33000433+reint-fischer@users.noreply.github.com>
2019-11-06 14:08:15 +0100

    Giovanni's changes

--- findpairs.py
+++ findpairs.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+#-------- FUNCTIONS -------
+
+#---> Selecting rule and thresholds for the pair formation
 def SelectPair(D):
     Pair= np.zeros([100,2])
     k=0
     for i in range ( int(len(D[2,:])/4) ): 
-        print( 100*(i*1.0)/(1.0*int(len(D[2,:])/4)), '%' )
+        print( 100*(i*1.0)/(1.0*int(len(D[2,:])/4)), '%' ) # Percentage of the programm calculations
         for t in range ( int(len(D[2,:])/4) ):
             if D[0,i*4]!= D[0,t*4] : 
                 space_distance = (D[2,i*4]-D[2,t*4])*(D[2,i*4]-D[2,t*4]) + (D[1,i*4]-D[1,t*4])*(D[1,i*4]-D[1,t*4])
@@ -15,20 +18,21 @@ def SelectPair(D):
                        k = k+1
     return Pair
 
-Data_Mediterrean = np.genfromtxt('Data/MedSeaIDs.txt', delimiter=',')
+#------- MAIN -------
 
-Pair = SelectPair(Data_Mediterrean) #Find chance pairs
+Data_Mediterrean = np.genfromtxt('Data/MedSeaIDs.txt', delimiter=',')  # Open reanalysed data
+Pair = SelectPair(Data_Mediterrean)                               # Find pairs of the system 
 
 k = 0 
-UnPair = np.zeros([50,2])
-for i in range (0,len(Pair)):
+UnPair = np.zeros([49,2])
+for i in range (len(Pair)):
     A = Pair[i]
     B = [A[1] ,A[0]]
-    if not (B in Pair): print('Fuck it') #There's always another
+    if not (B in Pair): print('Flag') #There's always another
     if not( A in UnPair) : 
        if not ( B in UnPair) : 
           UnPair[k] = A
           k= k+1
 
-np.savetxt('Data/Pair.txt',Pair,delimiter=',')  #Save chance pair IDs
-np.savetxt('Data/UnPair.txt',UnPair,delimiter=',')
+np.savetxt('Data/Pair.txt',Pair,delimiter=',')      #Save the IDs (iterations)
+np.savetxt('Data/UnPair.txt',UnPair,delimiter=',')  #Save the IDs (couples)
